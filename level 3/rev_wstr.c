ed01fbf2848cae01c2a4de2a5701afe899984500
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-07-10 14:55:39 +0100

    Update rev_wstr.c

--- level 3/rev_wstr.c
+++ level 3/rev_wstr.c
@@ -1,30 +1,34 @@
-#include <stdlib.h>
 #include <unistd.h>
 
 int	main(int argc, char **argv)
 {
+	int	start;
+	int	end;
 	int	i;
-	int	j;
-
-	if(argc == 2)
+	int	flag;
+	
+	i = 0;
+	if (argc == 2)
 	{
-		i = 0;
-		j = 0;
 		while (argv[1][i])
 			i++;
-		while (i > 0)
+		while (i >= 0)
 		{
-			while (argv[1][i] != ' ' && argv[1][i] != '\t' && i > 0)
+			while (argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
 				i--;
-			j = i--;
-			if (argv[1][j] == ' ' && argv[1][j] == '\t')
-				j++;
-			while (argv[1][j] && argv[1][j] != ' ' && argv[1][j] != '\t')
-				write(1, &argv[1][j++], 1);
-			if (i > 0)
+			end = i;
+			while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+				i--;
+			start = i + 1;
+			flag = start;
+			while (start <= end)
+			{
+				write(1, &argv[1][start], 1);
+				start++;
+			}
+			if (flag != 0)
 				write(1, " ", 1);
 		}
-
 	}
 	write(1, "\n", 1);
 }
