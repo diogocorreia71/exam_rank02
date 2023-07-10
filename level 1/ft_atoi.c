3bc44873b18020b1329a0c84333d3dd54979dd8d
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-07-10 15:28:19 +0100

    Update ft_atoi.c

--- level 1/ft_atoi.c
+++ level 1/ft_atoi.c
@@ -12,20 +12,29 @@
 
 int	ft_atoi(const char *str)
 {
-	int	res;
 	int	i;
+	int	res;
 	int	sign;
 
-	res = 0;
 	i = 0;
+	res = 0;
 	sign = 1;
-	if (str[0] == '-' || str[0] == '+')
-	{
-		if (str[0] == '-')
-			sign = -1;
-		i++;
-	}
 	while (str[i])
-		res = res * 10 + str[i++] - 48;
+	{
+		if ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+			i++;
+		if (str[i] == '-')
+		{
+			sign = -1;
+			i++;
+		}
+		else if (str[i] == '+')
+			i++;
+		while (str[i] >= '0' && str[i] <= '9')
+		{
+			res = res * 10 + (str[i] - '0');
+			i++;
+		}
+	}
 	return (res * sign);
 }
