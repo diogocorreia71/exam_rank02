9bf3fc2b1339cb0fdb65bac74cb3124af1c0aa67
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-07-24 10:44:51 +0100

    Update search_and_replace.c

--- level 0/search_and_replace.c
+++ level 0/search_and_replace.c
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	int	i;
 	
 	i = 0;
-	if (argc == 4 && !argv[2][1] && !argv[3][1])
+	if (argc == 4)
 	{
-		while (argv[1][i])
+		while (argv[1][i] && !argv[2][1] && !argv[3][1])
 		{
 			if(argv[1][i] == argv[2][0])
 				write(1, argv[3][0], 1);
@@ -29,5 +29,4 @@ int	main(int argc, char **argv)
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
