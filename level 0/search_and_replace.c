376ad69b5de72408a125a4a98d8628acc12e0068
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-07-24 11:26:54 +0100

    Update search_and_replace.c

--- level 0/search_and_replace.c
+++ level 0/search_and_replace.c
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 			if(argv[1][i] == argv[2][0])
 				write(1, argv[3][0], 1);
 			else
-				write(1, argv[1][i], 1);
+				write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
