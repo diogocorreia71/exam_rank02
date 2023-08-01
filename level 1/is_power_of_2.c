a357cc958090d15ab02b33308abfd638f5b16041
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-01 10:49:15 +0100

    Update is_power_of_2.c

--- level 1/is_power_of_2.c
+++ level 1/is_power_of_2.c
@@ -5,20 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/21 12:05:52 by diodos-s          #+#    #+#             */
-/*   Updated: 2023/06/21 12:11:13 by diodos-s         ###   ########.fr       */
+/*   Created: 2023/08/01 10:39:25 by diodos-s          #+#    #+#             */
+/*   Updated: 2023/08/01 10:47:33 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_power_of_2(unsigned int n)
+int	    is_power_of_2(unsigned int n)
 {
-	while (n > 0)
-	{
-		if (n == 2)
-			return (1);
-		if (n % 2 != 0)
-			return (0);
-		n = n / 2;
-	}
+	if (n == 0)
+		return (0);
+	while (n % 2 == 0)
+		n /= 2;
+	if (n == 1)
+		return (1);
 	return (0);
 }
