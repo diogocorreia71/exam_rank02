44aba8c51c86a0b018b3b1458012443e31fcfc3e
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-01 10:29:21 +0100

    Update fprime.c

--- level 3/fprime.c
+++ level 3/fprime.c
@@ -1,12 +1,24 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 10:23:41 by diodos-s          #+#    #+#             */
+/*   Updated: 2023/08/01 10:28:45 by diodos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int	ft_isprime(int n)
 {
-	int	i;
+	int i;
 
 	i = 2;
-	while (n > i)
+	if (n > i)
 	{
 		if (n % i == 0)
 			return (0);
@@ -15,12 +27,12 @@ int	ft_isprime(int n)
 	return (1);
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	n;
-	int	i;
-	int	first;
-	
+	int n;
+	int i;
+	int first;
+
 	i = 2;
 	first = 1;
 	if (argc == 2)
@@ -48,5 +60,4 @@ int	main(int argc, char **argv)
 		}
 	}
 	printf("\n");
-	return (0);
 }
