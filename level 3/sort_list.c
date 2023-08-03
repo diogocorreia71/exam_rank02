fa977a2bb1bdc651832b9ad8688f21699ae53cc7
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-03 12:46:59 +0100

    Update sort_list.c

--- level 3/sort_list.c
+++ level 3/sort_list.c
@@ -5,20 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/26 09:17:15 by diodos-s          #+#    #+#             */
-/*   Updated: 2023/06/26 09:25:19 by diodos-s         ###   ########.fr       */
+/*   Created: 2023/08/03 12:40:49 by diodos-s          #+#    #+#             */
+/*   Updated: 2023/08/03 12:46:07 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "list.h"
 
-t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-	int	temp;
-	t_list	*curr;
-
+	int temp;
+	t_list *curr;
+	
 	temp = 0;
 	curr = lst;
 	while (lst->next)
