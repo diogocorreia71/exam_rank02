fcae0abbf8b57880ae6b9c56aaf5f50d4b6f2e07
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-03 11:05:35 +0100

    Update is_power_of_2.c

--- level 1/is_power_of_2.c
+++ level 1/is_power_of_2.c
@@ -14,9 +14,9 @@ int	    is_power_of_2(unsigned int n)
 {
 	if (n == 0)
 		return (0);
-	while (n % 2 == 0)
-		n /= 2;
-	if (n == 1)
+	else if (n % 2 == 0)
+		return (1);
+	else if (n == 1)
 		return (1);
 	return (0);
 }
