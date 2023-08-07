f0b6d32ef28781ba84dd2a62dbc00c8449763274
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-07 11:30:27 +0100

    Update ft_range.c

--- level 2/ft_range.c
+++ level 2/ft_range.c
@@ -5,17 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/22 12:14:59 by diodos-s          #+#    #+#             */
-/*   Updated: 2023/06/22 12:20:16 by diodos-s         ###   ########.fr       */
+/*   Created: 2023/08/07 11:12:51 by diodos-s          #+#    #+#             */
+/*   Updated: 2023/08/07 11:26:31 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	*ft_range(int start, int end)
+int     *ft_range(int start, int end)
 {
-	int size = abs(end - start) + 1;
-	int *res = malloc(sizeof(int) * size);
+	int size = (end - start);
+	if (size < 0)
+	{
+		size *= -1;
+	}
+	size = size + 1;
+	int *res = malloc(sizeof(int) * (size));
 	if (!res)
 		return NULL;
 	int i = 0;
