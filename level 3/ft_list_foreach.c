4454dd21adc890cc80ae0380b96382f9c2fd904b
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-10 08:34:00 +0100

    Update ft_list_foreach.c

--- level 3/ft_list_foreach.c
+++ level 3/ft_list_foreach.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include "ft_list.h"
+
 typedef struct    s_list
 {
 	struct s_list *next;
@@ -8,7 +11,7 @@ void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
 	while (begin_list)
 	{
-		f(begin_list->data);
+		(*f)(begin_list->data);
 		begin_list = begin_list->next;
 	}
 }
