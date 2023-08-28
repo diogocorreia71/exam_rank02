7336ac389c55e6dafd70ab84b95cce7af5e6546f
Diogo Correia <118314025+diogocorreia71@users.noreply.github.com>
2023-08-28 10:36:51 +0100

    Update epur_str.c

--- level 2/epur_str.c
+++ level 2/epur_str.c
@@ -12,18 +12,15 @@
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i;
-	int	flag;
-
 	if (argc == 2)
 	{
-		i = 0;
-		flag = 0;
+		int i = 0;
+		int flag = 0;
 		while (argv[1][i])
 		{
-			while (argv[1][i] && (argv[1][i] == '\t' || argv[1][i] == ' '))
+			while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
 				i++;
 			if (argv[1][i] && flag)
 				write(1, " ", 1);
@@ -32,10 +29,7 @@ int	main(int argc, char **argv)
 				flag = write(1, &argv[1][i], 1);
 				i++;
 			}
-			if (argv[1][i])
-				i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
